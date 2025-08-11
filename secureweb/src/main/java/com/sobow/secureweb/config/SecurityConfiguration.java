558db5feacf1815e8d680fcd701b0706831a735d
Patryk Sobow <patryk.sobow95@gmail.com>
2025-08-11 08:18:48 +0200

    Add spring security default configuration

---
+++ secureweb/src/main/java/com/sobow/secureweb/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.sobow.secureweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest()
+                                                       .authenticated())
+            .formLogin(form -> form.permitAll())
+            .logout(logout -> logout.permitAll())
+            .httpBasic(basic -> {
+            });
+        
+        return httpSecurity.build();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                               .username("user")
+                               .password(passwordEncoder().encode("password"))
+                               .roles("USER")
+                               .build();
+        
+        return new InMemoryUserDetailsManager(user);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
