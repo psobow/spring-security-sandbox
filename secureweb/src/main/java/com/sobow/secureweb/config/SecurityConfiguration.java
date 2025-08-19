2d199dde08a1d5574532366c285804c62f7cd152
Patryk Sobow <patryk.sobow95@gmail.com>
2025-08-19 18:32:39 +0200

    Add managing users in database

--- secureweb/src/main/java/com/sobow/secureweb/config/SecurityConfiguration.java
+++ secureweb/src/main/java/com/sobow/secureweb/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.sobow.secureweb.config;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,9 @@ import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
@@ -66,7 +66,8 @@ public class SecurityConfiguration {
     }
     
     @Bean
-    public UserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        /*
         UserDetails userOne = User.builder()
                                .username("user")
                                .password(passwordEncoder().encode("password"))
@@ -80,6 +81,19 @@ public class SecurityConfiguration {
                                   .build();
         
         return new InMemoryUserDetailsManager(userOne, userTwo);
+         */
+        
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        
+        if (!jdbcUserDetailsManager.userExists("user")) {
+            UserDetails admin = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+            jdbcUserDetailsManager.createUser(admin);
+        }
+        return jdbcUserDetailsManager;
     }
     
     @Bean
